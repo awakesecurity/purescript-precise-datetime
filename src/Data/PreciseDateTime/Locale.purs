6d34a0a191a601482c511a5a268214261500ae42
Matthew Chan <matt@awakenetworks.com>
2018-02-26 14:04:54 -0800

    use do notation

--- src/Data/PreciseDateTime/Locale.purs
+++ src/Data/PreciseDateTime/Locale.purs
@@ -6,14 +6,12 @@ import Data.DateTime.Locale (LocalValue(..), LocalDateTime)
 import Data.Maybe (Maybe)
 import Data.PreciseDateTime (PreciseDateTime)
 import Data.PreciseDateTime as PDT
-import Data.Profunctor.Strong ((&&&))
 import Data.RFC3339String (RFC3339String(..))
 import Data.RFC3339String as RFC3339String
 import Data.RFC3339String.Format (formatLocale)
 import Data.String (dropRight)
 import Data.Time.PreciseDuration (PreciseDuration)
-import Data.Traversable (sequence, traverse)
-import Data.Tuple (uncurry)
+import Data.Traversable (traverse)
 
 type LocalPreciseDateTime = LocalValue PreciseDateTime
 
@@ -21,9 +19,10 @@ adjust :: PreciseDuration -> LocalPreciseDateTime -> Maybe LocalPreciseDateTime
 adjust = traverse <<< PDT.adjust
 
 fromRFC3339String :: RFC3339String -> Maybe LocalPreciseDateTime
-fromRFC3339String = map (uncurry LocalValue)
-                    <<< sequence
-                    <<< (RFC3339String.toLocale &&& PDT.fromRFC3339String)
+fromRFC3339String = do
+  loc <- RFC3339String.toLocale
+  pdt <- PDT.fromRFC3339String
+  pure $ LocalValue loc <$> pdt
 
 toRFC3339String :: LocalPreciseDateTime -> RFC3339String
 toRFC3339String (LocalValue locale pdt) =
