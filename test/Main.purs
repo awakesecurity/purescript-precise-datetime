0fbe8d607b051417d5dab4da4ff9adffd3e4ac13
Thomas Honeyman <admin@thomashoneyman.com>
2021-03-04 13:31:56 -0800

    Update for PureScript 0.14 (#34)
    * Update bower dependencies
    
    * Update bower repository url
    
    * Use thomashoneyman fork of decimals temporarily
    
    * Update for unicode changes
    
    * Fix tests
    
    * Update CI to use PureScript 0.14
    
    * Update bower.json

--- test/Main.purs
+++ test/Main.purs
@@ -1,10 +1,14 @@
 module Test.Main where
 
 import Prelude
+
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Test.Spec.Discovery (discover)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = discover "Test\\..*\\.Spec" >>= run [consoleReporter]
+main = launchAff_ do
+  spec <- discover "Test\\..*\\.Spec"
+  runSpec [consoleReporter] spec
